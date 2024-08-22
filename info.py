9e18daea3045b646dd4a96d313c386b92946565e
Sharon12345g <104382026+Sharon12345g@users.noreply.github.com>
2024-08-22 13:19:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '754e27280d4e8c5bed3dd35ed84fc1630c
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 PORT = environ.get("PORT", "8080")
-MSG_ALRT = environ.get('MSG_ALRT', 'Asʜᴜᴛᴏsʜ Gᴏsᴡᴀᴍɪ 𝟸𝟺 🇮🇳')
+MSG_ALRT = environ.get('MSG_ALRT', 'Movies4U')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
