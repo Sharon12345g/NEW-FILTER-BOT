92f2d6dafd4af250f3dcc39d16bea27f0905cd4b
Sharon12345g <104382026+Sharon12345g@users.noreply.github.com>
2024-10-11 12:34:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://reimagined-goggles-97qx7x47qgrqfpj5-8080.app.github.dev/")
+URL = environ.get("URL", "")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
