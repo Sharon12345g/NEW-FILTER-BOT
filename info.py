c51758672f47b199b4a22b01d5301eac86b050fa
Sharon12345g <104382026+Sharon12345g@users.noreply.github.com>
2024-10-26 16:57:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://my-filter-bot.onrender.com")
+URL = environ.get("URL", "https://movies4u-top.onrender.com")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
